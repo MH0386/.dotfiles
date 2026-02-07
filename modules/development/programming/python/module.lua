f2b6ddf659afb9eb155d9c1f6cbcbae1e1071db4
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2026-02-07 17:24:29 +0200

    feat: add ruff and ty to Python development tools

--- modules/development/programming/python/module.lua
+++ modules/development/programming/python/module.lua
@@ -2,7 +2,9 @@ return {
     description = "Python development tools",
     packages = {
         "uv",
-        "python-uv"
+        "python-uv",
+        "ruff",
+        "ty"
     },
     dotfiles_sync = true
 }
